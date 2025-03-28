16eb48710be718406317522728ca6931734cbc77
Romain Avonde <romain@avonde.email>
2025-03-28 09:46:55 +0100

    🆕 Add search and article management features
    ✨ Added a new search functionality for articles with a loading indicator.
    ✨ Introduced separate view models for searching and adding articles.
    ✨ Implemented filtering logic based on embedding thresholds in the database layer.
    ⚡ Enhanced UI components to improve user experience during article analysis.

--- src/Watch.Manager.Service.Database/ArticleAnalyseStore.cs
+++ src/Watch.Manager.Service.Database/ArticleAnalyseStore.cs
@@ -10,8 +10,10 @@ using Watch.Manager.Service.Database.Abstractions;
 using Watch.Manager.Service.Database.Context;
 using Watch.Manager.Service.Database.Entities;
 
-internal class ArticleAnalyseStore(ILogger<ArticleAnalyseStore> logger, ArticlesContext articlesContext) : IArticleAnalyseStore
+internal sealed class ArticleAnalyseStore(ILogger<ArticleAnalyseStore> logger, ArticlesContext articlesContext) : IArticleAnalyseStore
 {
+    private const double Threshold = 0.75;
+
     public async Task StoreArticleAnalyzeAsync(Article analyzeModel, CancellationToken cancellationToken)
     {
         _ = articlesContext.Articles.Add(analyzeModel);
@@ -27,11 +29,21 @@ internal class ArticleAnalyseStore(ILogger<ArticleAnalyseStore> logger, Articles
         IQueryable<Article> orderedQueryable = articlesContext.Articles;
 
         if (embedding.Length > 0)
-            orderedQueryable = orderedQueryable.OrderBy(p => p.EmbeddingBody.CosineDistance(embeddingVector)).ThenBy(p => p.EmbeddingHead.CosineDistance(embeddingVector));
+        {
+            //var test = orderedQueryable.Select(a => new
+            //{
+            //    a.Title,
+            //    Body = a.EmbeddingBody.CosineDistance(embeddingVector),
+            //    Head = a.EmbeddingHead.CosineDistance(embeddingVector),
+            //}).ToList();
+            orderedQueryable = orderedQueryable
+                              .Where(p => p.EmbeddingBody.CosineDistance(embeddingVector) < Threshold && p.EmbeddingHead.CosineDistance(embeddingVector) < Threshold)
+                              .OrderBy(p => p.EmbeddingBody.CosineDistance(embeddingVector)).ThenBy(p => p.EmbeddingHead.CosineDistance(embeddingVector));
+        }
 
 
         return await orderedQueryable
-                    .Take(5)
+                    .OrderByDescending(a => a.AnalyzeDate)
                     .ToArrayAsync(cancellationToken).ConfigureAwait(false);
     }
 }
--- src/Watch.Manager.Web/Components/Pages/Home.razor.cs
+++ src/Watch.Manager.Web/Components/Pages/Home.razor.cs
@@ -15,21 +15,23 @@ using Watch.Manager.Web.Services.Models;
 /// </summary>
 public partial class Home
 {
-    private readonly IToastService toastService;
-    private readonly AnalyzeService analyzeService;
-    private readonly ArticleViewModel viewModel = new();
-    private ExtractAnalyseModel? analyseResponse;
-    private ArticleModel[] articles = [];
-    private bool analyzeInProgress;
-
     private static readonly MarkdownPipeline MarkdownPipeline = new MarkdownPipelineBuilder()
                                                                .UseAdvancedExtensions()
                                                                .UseColorCode()
                                                                .Build();
 
+    private readonly IToastService toastService;
+    private readonly AnalyzeService analyzeService;
+    private readonly SearchArticleViewModel searchArticleViewModel = new();
+    private readonly AddArticleViewModel addArticleViewModel = new();
+    private ArticleModel[] articles = [];
+    private bool analyzeInProgress;
+    private bool searchInProgress;
+
     /// <summary>
     ///     Initializes a new instance of the <see cref="Home" /> class.
     /// </summary>
+    /// <param name="toastService">The toast service.</param>
     /// <param name="analyzeService">The analyze service.</param>
     public Home(IToastService toastService, AnalyzeService analyzeService)
     {
@@ -44,24 +46,46 @@ public partial class Home
     private async Task ReloadArticlesAsync()
         => this.articles = await this.analyzeService.SearchArticleAsync(string.Empty).ConfigureAwait(true);
 
+    private async Task SearchArticleHandlerAsync()
+    {
+        this.searchInProgress = true;
+        var apiResult = await this.analyzeService.SearchArticleAsync(this.searchArticleViewModel.Terms).ConfigureAwait(true);
+
+        if (apiResult.Length == 0)
+        {
+            this.toastService.ShowWarning("Aucun résultat");
+            this.articles = [];
+            this.searchInProgress = false;
+            return;
+        }
+
+        this.articles = apiResult;
+        this.searchInProgress = false;
+    }
+
     private async Task SaveArticleHandlerAsync()
     {
         this.analyzeInProgress = true;
-        var apiResult = await this.analyzeService.SaveArticleAsync(this.viewModel.Url).ConfigureAwait(true);
+        var apiResult = await this.analyzeService.SaveArticleAsync(this.addArticleViewModel.Url).ConfigureAwait(true);
 
         if (apiResult.ApiResultErrorType == ApiResultErrorType.Conflict)
         {
             this.analyzeInProgress = false;
-            this.toastService.ShowWarning("Url already save");
+            this.toastService.ShowWarning("Url déjà sauvegardée");
             return;
         }
 
-        this.analyseResponse = apiResult.Result;
         await this.ReloadArticlesAsync().ConfigureAwait(true);
+        this.addArticleViewModel.Url = string.Empty;
         this.analyzeInProgress = false;
     }
 
-    private sealed class ArticleViewModel
+    private sealed class SearchArticleViewModel
+    {
+        public string Terms { get; set; } = string.Empty;
+    }
+
+    private sealed class AddArticleViewModel
     {
         public string Url { get; set; } = string.Empty;
     }
