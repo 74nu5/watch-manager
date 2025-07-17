14b0d58ade10da0d75b71faa03409f6262750536
Romain Avonde <romain@avonde.email>
2025-07-17 16:48:47 +0200

    Adds article component to improve article display
    ✨ Introduces a dedicated component for articles to simplify the home page and enhance code reuse
    🌟 Implements markdown summary with toggleable full/short view for better content readability
    🎨 Adds UI features like badges for tags, persona for authors, and external links with icons
    🔧 Cleans up styling and refines image presentation with subtle blur effects for polish
    🧩 Extracts article rendering logic from the main page to boost maintainability and scalability

---
+++ src/Watch.Manager.Web/Components/Article.razor.cs
@@ -0,0 +1,45 @@
+﻿namespace Watch.Manager.Web.Components;
+
+using Markdig;
+
+using Markdown.ColorCode;
+
+using Microsoft.AspNetCore.Components;
+
+using Watch.Manager.Web.Services.Models;
+
+public partial class Article : ComponentBase
+{
+    private static readonly MarkdownPipeline MarkdownPipeline = new MarkdownPipelineBuilder()
+                                                               .UseAdvancedExtensions()
+                                                               .UseColorCode()
+                                                               .Build();
+
+    private readonly IConfiguration configuration;
+    private bool showMore;
+
+    /// <inheritdoc />
+    public Article(IConfiguration configuration)
+        => this.configuration = configuration;
+
+    [Parameter]
+    public ArticleModel ArticleModel { get; set; }
+
+    private string ApiUrl => this.configuration.GetValue("services:apiservice:https:0", string.Empty);
+
+    private string GetFullMarkdownContent(string content)
+        => Markdown.ToHtml(content, MarkdownPipeline);
+
+    private string GetShortMarkdownContent(string content)
+    {
+        const int Length = 300;
+
+        if (string.IsNullOrEmpty(content) || content.Length <= Length)
+            return content;
+
+        return Markdown.ToHtml(content[..Length] + "...", MarkdownPipeline);
+    }
+
+    private void ToggleShowMore()
+        => this.showMore = !this.showMore;
+}
